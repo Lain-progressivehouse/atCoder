8a5dbdbf1fde1f4676c20d546a1be6cd0e890775
Lain-progressivehouse <lain.progressivehouse@gmail.com>
2019-11-20 15:58:02 +0900

    solve dp contest k

--- atCoder/dp_contest.py
+++ atCoder/dp_contest.py
@@ -254,5 +254,32 @@ def p_i():
     print(sum(dp[-1][(n + 1) // 2:]))
 
 
+"""
+J問題: Sushi
+難しそうなので後回し
+"""
+
+
+def p_j():
+    pass
+
+
+"""
+K問題: Stones
+"""
+
+
+def p_k():
+    n, k = map(int, input().split())
+    *a, = map(int, input().split())
+
+    dp = [False] * (k + 1)
+    for i in range(1, k + 1):
+        for j in a:
+            if i - j >= 0:
+                dp[i] |= not dp[i - j]
+    print("First" if dp[k] else "Second")
+
+
 if __name__ == '__main__':
-    p_i()
+    p_k()

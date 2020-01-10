59012193ab8971f4343c34f41cd2988c0e2c5b86
Lain-progressivehouse <lain.progressivehouse@gmail.com>
2020-01-10 20:57:12 +0900

    solved abc144 e

--- atCoder/abc144.py
+++ atCoder/abc144.py
@@ -40,7 +40,30 @@ def p_d():
 
 
 def p_e():
-    pass
+    n, k = map(int, input().split())
+    *A, = map(int, input().split())
+    *F, = map(int, input().split())
+    A.sort()
+    F.sort(reverse=True)
+
+    def check(x):
+        count = 0
+        for a, f in zip(A, F):
+            count += max(a - x // f, 0)
+        return count <= k
+
+    max_num = 0
+    for a, f in zip(A, F):
+        if max_num < a * f:
+            max_num = a * f
+    l, h = 0, max_num
+    while l <= h:
+        mid = (l + h) // 2
+        if check(mid):
+            h = mid - 1
+        else:
+            l = mid + 1
+    print(h + 1)
 
 
 def p_f():
@@ -48,4 +71,4 @@ def p_f():
 
 
 if __name__ == '__main__':
-    p_d()
+    p_e()

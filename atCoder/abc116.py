24738d4dce3af832cd10877e0f776a58afd2f0f8
Lain-progressivehouse <lain.progressivehouse@gmail.com>
2020-01-13 13:26:16 +0900

    solved abc116 d

--- atCoder/abc116.py
+++ atCoder/abc116.py
@@ -42,19 +42,33 @@ def p_c():
 
 def p_d():
     N, K = map(int, input().split())
-    li = []
+    inf = 10 ** 18
+    sushi = [[-inf] for _ in range(N)]
+    max_kind = -1
     for _ in range(N):
         t, d = map(int, input().split())
-        li.append((t, d))
-    li.sort(key=lambda x: x[1], reverse=True)
-    kind = set()
-    ans = 0
+        sushi[t - 1].append(d)
+        if max_kind < t:
+            max_kind = t
+    for i in range(max_kind):
+        sushi[i].sort(reverse=True)
+    sushi.sort(key=lambda x: x[0], reverse=True)
+
+    que = []
+    res = 0
     for i in range(K):
-        ans += li[i][1]
-        if li[i][0] not in kind:
-            kind.add(li[i][0])
-    ans += len(kind) ** 2
-    print(ans)
+        res += sushi[i][0]
+        for j in sushi[i][1:]:
+            que.append(j)
+    que.sort()
+    cur = res
+    res += K ** 2
+    for var in range(1, K)[::-1]:
+        a = que.pop()
+        b = sushi[var][0]
+        cur += a - b
+        res = max(res, cur + var ** 2)
+    print(res)
 
 
 if __name__ == '__main__':

ad765787fc804b444a7e48e22bb8a694b7fa2429
Lain-progressivehouse <lain.progressivehouse@gmail.com>
2020-01-22 15:19:45 +0900

    solved abc112

--- atCoder/abc112.py
+++ atCoder/abc112.py
@@ -22,15 +22,25 @@ def p_c():
     from itertools import product
     N = int(input())
     xyh = [tuple(map(int, input().split())) for _ in range(N)]
+    h = 0
+    for i in xyh:
+        if h < i[2]:
+            x_, y_, h = i
     for x, y in product(range(101), repeat=2):
-        high = xyh[0][2] + abs(xyh[0][0] - x) + abs(xyh[0][1] - y)
+        high = h + abs(x_ - x) + abs(y_ - y)
         if all(h == max(0, high - abs(x - i) - abs(y - j)) for i, j, h in xyh):
             print(x, y, high)
 
 
 def p_d():
-    pass
+    n, m = map(int, input().split())
+    max_candidate = m // n
+    for i in reversed(range(1, max_candidate + 1)):
+        diff = m - n * i
+        if diff % i == 0:
+            print(i)
+            break
 
 
 if __name__ == '__main__':
-    p_c()
+    p_d()
